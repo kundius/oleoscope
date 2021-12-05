590a5c3da4c706fafc7298519351d93e782663aa
Kundius Ruslan <kundius.ruslan@gmail.com>
2021-12-06 00:08:01 +0300

    u

--- functions.php
+++ functions.php
@@ -473,6 +473,7 @@ add_filter('nav_menu_css_class', 'current_type_nav_class', 10, 2);
 function current_type_nav_class($classes, $item) {
     // Get post_type for this post
     $post_type = get_query_var('post_type');
+		print_r($post_type, $item);
 
     // Go to Menus and add a menu class named: {custom-post-type}-menu-item
     // This adds a 'current_page_parent' class to the parent menu item
